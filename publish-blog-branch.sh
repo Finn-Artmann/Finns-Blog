b012459bb8bb0463e57b85f437d4078ead89603f
Finn Artmann <finn.artmann@web.de>
2024-12-11 22:03:29 +0100

    Update deploy script to also push to main branch

--- publish-blog-branch.sh
+++ publish-blog-branch.sh
@@ -1,7 +1,8 @@
 hugo
 sleep 2
 git add public assets content resources
-git commit -m "Add changes in public, assets, content and resources folders after Hugo build"
+git commit -m "Add changes in public, assets, content, and resources folders after Hugo build"
+git push origin main
 git subtree split --prefix public -b blog-deploy
 git push origin blog-deploy:blog --force
 git branch -D blog-deploy
