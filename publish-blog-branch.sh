c0973ba0c4c0218050e055456c7c5b2f796aca51
Finn Artmann <finn.artmann@web.de>
2024-12-11 22:01:53 +0100

    Update publish script to automatically commit changes to blog sites

--- publish-blog-branch.sh
+++ publish-blog-branch.sh
@@ -1,4 +1,7 @@
 hugo
+sleep 2
+git add public assets content resources
+git commit -m "Add changes in public, assets, content and resources folders after Hugo build"
 git subtree split --prefix public -b blog-deploy
 git push origin blog-deploy:blog --force
 git branch -D blog-deploy
